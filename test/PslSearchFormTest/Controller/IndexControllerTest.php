974058e61a79aeef402382880d1d16cbf17936fe
Julian Maurice <julian.maurice@biblibre.com>
2017-05-29 12:49:58 +0200

    Call dispatch() only once per test

--- test/PslSearchFormTest/Controller/IndexControllerTest.php
+++ test/PslSearchFormTest/Controller/IndexControllerTest.php
@@ -6,17 +6,28 @@ use PslSearchFormTest\Controller\PslSearchFormControllerTestCase;
 
 class IndexControllerTest extends PslSearchFormControllerTestCase
 {
+    protected $site;
+
     public function setUp()
     {
         parent::setUp();
 
-        $this->api()->create('sites', [
+        $response = $this->api()->create('sites', [
             'o:title' => 'Test site',
             'o:slug' => 'test',
             'o:theme' => 'default',
         ]);
 
+        $this->site = $response->getContent();
+
         $this->resetApplication();
+    }
+
+    public function tearDown()
+    {
+        $this->api()->delete('sites', $this->site->id());
+
+        parent::tearDown();
     }
 
     public function testSearchAction()
@@ -26,7 +37,10 @@ class IndexControllerTest extends PslSearchFormControllerTestCase
 
         $this->assertQuery('input[name="q"]');
         $this->assertNotQuery('.search-results');
+    }
 
+    public function testSearchWithParamsAction()
+    {
         $this->dispatch('/s/test/test/search', 'GET', ['q' => 'test']);
         $this->assertResponseStatusCode(200);
         $this->assertQuery('.search-results');
